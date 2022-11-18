60bdf4b170e7ac37f67c48f87a39da371e9b62b5
CircuCarina <badita.carina@yahoo.com>
2022-11-18 15:30:48 +0200

    Laborator 2

---
+++ Circu_Carina_Lab2/Models/Publisher.cs
@@ -0,0 +1,9 @@
+﻿namespace Circu_Carina_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    } //navigation property
+}
